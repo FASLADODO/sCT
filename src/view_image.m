d16105c237acaed21fbb18951e8a46d75e13eeb5
chris <chris@Chris-PC>
2014-03-13 11:27:37 +0100

    normalize file paths

--- src/view_image.m
+++ src/view_image.m
@@ -1,4 +1,4 @@
 startup;
 
-nii = load_nii('test_patient\\Pet Hjerne_Lavdosis_Ct_6 (Adult)\AC_CT - 2\\o20130826_120110HjerneLavdosisCT6s002a002.nii');
+nii = load_nii('test_patient/Pet Hjerne_Lavdosis_Ct_6 (Adult)/AC_CT - 2/o20130826_120110HjerneLavdosisCT6s002a002.nii');
 view_nii(nii);
