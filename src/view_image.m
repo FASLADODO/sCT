e214f694b5816e4e4ee8426018c812591d68a723
chris <chris@Chris-PC>
2014-03-13 11:10:42 +0100

    add to filepath

--- src/view_image.m
+++ src/view_image.m
@@ -1,6 +1,4 @@
-import toolbox.*;
+startup;
 
-
-
-load_nii('test_patient\\Pet Hjerne_Lavdosis_Ct_6 (Adult)\AC_CT - 2\\o20130826_120110HjerneLavdosisCT6s002a002.nii');
+nii = load_nii('test_patient\\Pet Hjerne_Lavdosis_Ct_6 (Adult)\AC_CT - 2\\o20130826_120110HjerneLavdosisCT6s002a002.nii');
 view_nii(nii);
