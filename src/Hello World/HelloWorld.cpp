4747c7c7853c96d18d3deced6e3dfe4ae474cbf2
chris <chris@Chris-PC>
2014-03-19 16:22:30 +0100

    HelloWorld now with CT data

--- src/Hello World/HelloWorld.cpp
+++ src/Hello World/HelloWorld.cpp
@@ -1,10 +1,74 @@
-#include "itkImage.h"
 #include <iostream>
+#include "itkImageFileReader.h"
 
-int main()
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+#include "itkExtractImageFilter.h"
+
+#include "QuickView.h"
+
+int main(int argc, const char* argv[])
 {
-	typedef itk::Image< unsigned short, 3 > ImageType;
-	ImageType::Pointer image = ImageType::New();
+	if (argc < 2)
+	{
+		return -1;
+	}
+
 	std::cout << "ITK Hello World !" << std::endl;
+
+	typedef float InputPixelType;
+	typedef float OutputPixelType;
+	typedef itk::Image< OutputPixelType, 2 > OutputImageType;
+
+	typedef itk::Image<InputPixelType, 3>  InputImageType;
+	typedef itk::ImageFileReader<InputImageType> ReaderType;
+	ReaderType::Pointer reader = ReaderType::New();
+
+	typedef itk::ExtractImageFilter< InputImageType,
+		OutputImageType > FilterType;
+	FilterType::Pointer filter = FilterType::New();
+	filter->InPlaceOn();
+	filter->SetDirectionCollapseToSubmatrix();
+
+	reader->SetFileName(argv[1]);
+	reader->Update();
+
+
+	InputImageType* inputImage = reader->GetOutput();
+
+	InputImageType::RegionType inputRegion =
+		reader->GetOutput()->GetLargestPossibleRegion();
+
+	InputImageType::SizeType size = inputRegion.GetSize();
+	size[2] = 0;
+
+	InputImageType::IndexType start = inputRegion.GetIndex();
+	const unsigned int sliceNumber = 50;
+	start[2] = sliceNumber;
+
+	InputImageType::RegionType desiredRegion;
+	desiredRegion.SetSize(size);
+	desiredRegion.SetIndex(start);
+
+	filter->SetExtractionRegion(desiredRegion);
+
+	filter->SetInput(inputImage);
+
+	OutputImageType* outputImage = filter->GetOutput();
+
+	typedef itk::RescaleIntensityImageFilter< OutputImageType, OutputImageType > RescaleFilterType;
+	RescaleFilterType::Pointer rescaleFilter = RescaleFilterType::New();
+	rescaleFilter->SetInput(outputImage);
+	rescaleFilter->SetOutputMinimum(0);
+	rescaleFilter->SetOutputMaximum(255);
+
+	QuickView viewer;
+	viewer.AddImage(outputImage);
+	viewer.AddImage(rescaleFilter->GetOutput());
+	viewer.Visualize();
+
 	return 0;
 }
