bc2fd20bf10ba9ae5a5f13e75e06f088f104fe84
dread-muffin <dreadmuffin@gmail.com>
2014-04-26 15:23:01 +0200

    gen_all_sctv3

--- src/em_matlab/gen_all_sct.m
+++ src/em_matlab/gen_all_sct.m
@@ -6,46 +6,46 @@ save(sct0,'sct0.nii');
 toc;
 
 tic;
-sct1 = gen_sct_func( p0, '010769/coreg/mask.nii');
+sct1 = gen_sct_func( p1, '010769/coreg/mask.nii');
 save(sct1,'sct1.nii');
 toc;
 
 tic;
-sct2 = gen_sct_func( p0, '010850/coreg/mask.nii');
+sct2 = gen_sct_func( p2, '010850/coreg/mask.nii');
 save(sct2,'sct2.nii');
 toc;
 
 tic;
-sct3 = gen_sct_func( p0, '010960/coreg/mask.nii');
+sct3 = gen_sct_func( p3, '010960/coreg/mask.nii');
 save(sct3,'sct3.nii');
 toc;
 
 tic;
-sct4 = gen_sct_func( p0, '011030/coreg/mask.nii');
+sct4 = gen_sct_func( p4, '011030/coreg/mask.nii');
 save(sct4,'sct4.nii');
 toc;
 
 tic;
-sct5 = gen_sct_func( p0, '020879/coreg/mask.nii');
+sct5 = gen_sct_func( p5, '020879/coreg/mask.nii');
 save(sct5,'sct5.nii');
 toc;
 
 tic;
-sct6 = gen_sct_func( p0, '020939/coreg/mask.nii');
+sct6 = gen_sct_func( p6, '020939/coreg/mask.nii');
 save(sct6','sct6.nii');
 toc;
 
 tic;
-sct7 = gen_sct_func( p0, '030242/coreg/mask.nii');
+sct7 = gen_sct_func( p7, '030242/coreg/mask.nii');
 save(sct7,'sct7.nii');
 toc;
 
 tic;
-sct8 = gen_sct_func( p0, '030330/coreg/mask.nii');
+sct8 = gen_sct_func( p8, '030330/coreg/mask.nii');
 save(sct8,'sct8.nii');
 toc;
 
 tic;
-sct9 = gen_sct_func( p0, '040145/coreg/mask.nii');
+sct9 = gen_sct_func( p9, '040145/coreg/mask.nii');
 save(sct9,'sct9.nii');
 toc;
