25c3f6e6a8227050d1ba25075e1ab061a008dd39
chris <chris@Chris-PC>
2014-04-28 02:22:02 +0200

    can compute percent diff

---
+++ src/em_matlab/percent_diff.m
@@ -0,0 +1,11 @@
+patient = '010476';
+ct = load_nii(strcat(patient,'/coreg/ct.nii'));
+sct = load_nii(strcat(patient,'/coreg/sct.nii'));
+ct_max = max( reshape( ct.img, numel( ct.img ), 1 ) );
+ct_norm = ct.img ./ ct_max;
+sct_max = max( reshape( sct.img, numel( sct.img ), 1 ) );
+sct_norm = sct.img ./ sct_max;
+
+pdct = ( abs( (ct.img) - (sct.img) ) ./ ( ct.img + sct.img )./2 ) .* 100;
+
+save_nii(make_nii(pdct), 'pdct.nii');
